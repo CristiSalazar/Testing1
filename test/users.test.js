df9efa11cfe7479974b9d1cfb1f357024c23ab71
CristiSalazar <cristina.salazar125@gmail.com>
2024-01-09 20:19:14 -0300

    segundo commit

---
+++ test/users.test.js
@@ -0,0 +1,87 @@
+import User from "../src/dao/mongo/users.mongo.js"
+import config from "../src/config/config.js"
+import mongoose from "mongoose"
+import Assert from "assert"
+import Chai from "chai"
+// import Supertest from 'supertest'
+
+mongoose.connect(config.mongo_url);
+
+const assert = Assert.strict
+// const expect = Chai.expect
+// const requester = Supertest("http://localhost:8080")
+
+describe('Testing User DAO get method', () => {
+    before(function () {
+        this.usersDao = new User()
+    })
+
+    it("Debería devolver los usuarios de la DB", async function () {
+        this.timeout(5000)
+        try
+        {
+            const result = await this.usersDao.get()
+            assert.strictEqual(Array.isArray(result) && result.length >0 , true) 
+            // expect(Array.isArray(result)).to.be.equals(true) 
+        }
+        catch(error){
+            console.error("Error durante el test: ", error)
+            assert.fail("Test con error")
+        }
+    })
+
+    it("El DAO debe agregar a un usuario en la DB", async function () {
+        let mockUser = {
+            first_name: "Test Nombre",
+            last_name: "Test Apellido",
+            email: "Test Email",
+            age: 22,
+            password: "Test Contraseña",
+            rol: "Test Rol"
+        }
+        const result = await this.usersDao.save(mockUser)
+        assert.deepStrictEqual(result._id) 
+        // expect(result).to.have.property('_id') 
+    })
+
+    it("El DAO debe obtener un usuario por correo", async function () {
+        let emailToFind = "cristina.salazar125@gmail.com"
+        const result = await this.usersDao.findEmail({ email: emailToFind })
+        assert.strictEqual(typeof result, "object") 
+        // expect(result).to.be.an('object') 
+    })
+
+    it("El DAO debe devolver un usuario despues de colocar un parametro", async function () {
+        let filterData = { first_name: 'admin'}
+        const result = await this.usersDao.findJWT(filterData)
+        assert.strictEqual(typeof result, "object") 
+        // expect(result).to.be.an('array'); 
+    })
+
+    it("El endpoint GET /users debe devolver todos los usuarios", async function() {
+        const response = await requester.get('/users')
+        assert.strictEqual(response.status, 200);
+        expect(response.type).to.equal('application/json');
+        expect(response.body).to.have.property('status', 'success');
+    })
+    it("El endpoint POST /users debe crear un usuario", async function() {
+        let mockUser = {
+            first_name: "SuperTest Nombre",
+            last_name: "SuperTest Apellido",
+            email: "SuperTest Email",
+            age: 22,
+            password: "SuperTest Contraseña",
+            rol: "SuperTest Rol"
+        }
+        
+        const response = await requester.post('/users').send(mockUser)
+        assert.strictEqual(response.status, 200);
+        expect(response.ok).to.equal(true);
+        expect(response.body).to.have.property('status', 'success');
+    })
+    after(function(done) {
+        this.timeout(5000);
+        console.log("Fin de las pruebas de User");
+        done();
+    });
+})
